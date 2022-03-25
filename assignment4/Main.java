340c2f108e779092cb8fb3e97fe07da0ccca7daf
NguyetCam <96852028+NguyetCam@users.noreply.github.com>
2022-03-25 19:31:04 +0700

    Update Main.java

--- assignment4/Main.java
+++ assignment4/Main.java
@@ -38,11 +38,20 @@ public class Main {
         mhdc.danhSachLopHoc.get(0).danhSachLop = A1.danhSachLop;
 
         LopHoc A3 = new LopHoc("A3",4);
-        A3.themHocSinh("Cầm","A1");
-        A3.themHocSinh("Linh","A1");
-        A3.themHocSinh("Cường","A1");
-        A3.themHocSinh("Duy","A1");
+        A3.themHocSinh("Cầm","A3");
+        A3.themHocSinh("Linh","A3");
+        A3.themHocSinh("Cường","A3");
+        A3.themHocSinh("Duy","A3");
         mhdc.danhSachLopHoc.get(2).danhSachLop = A3.danhSachLop;
+
+        LopHoc A5 = new LopHoc("A5",3);
+        A5.themHocSinh("Cầm","A5");
+        A5.themHocSinh("Linh","A5");
+        A5.themHocSinh("Cầm","A5");
+        A5.themHocSinh("Cường","A5");
+        mhdc.danhSachLopHoc.get(1).danhSachLop = A5.danhSachLop;
+
+        //In danh sách học sinh
         mhdc.inDanhSach();
     }
 }
